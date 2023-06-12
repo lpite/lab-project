a65cd1c38d4a0d199c2e4f56b90ede391965b9a9
lpite <67955038+lpite@users.noreply.github.com>
2023-06-12 12:02:26 +0300

    Update subscribe.ts

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -30,29 +30,42 @@ export default async function (
   request: VercelRequest,
   response: VercelResponse
 ) {
-  if (request.method !== 'POST') {
-    response.status(404);
-  }
+  
+switch (request.method) {
+  case "POST":
+    const bodyValidator = zod.object({
+      name: zod.string().regex(/[А-Яа-яЇїЄє ]/g),
+      phoneNumber: zod.string().regex(/\+380\d{3}\d{2}\d{2}\d{2}/gi),
+      text: zod.string().min(1),
+    });
+    const body = bodyValidator.parse(request.body);
+    await db
+      .insertInto('feedback')
+      .values({
+        name: body.name,
+        phoneNumber: body.phoneNumber,
+        text: body.text,
+      })
+      .executeTakeFirstOrThrow();
+  
+    response.send({ success: true });
+    try {
+    } catch (error) {
+      console.error(error);
+      response.status(500).send({ success: false });
+    }
+    break;
+    case "GET":
+      const subscriptions =  await db.selectFrom("feedback")
+      .selectAll()
+      .execute()
+      return response.send(subscriptions);
+      break;
 
-  const bodyValidator = zod.object({
-    name: zod.string().regex(/[А-Яа-яЇїЄє ]/g),
-    phoneNumber: zod.string().regex(/\+380\d{3}\d{2}\d{2}\d{2}/gi),
-    text: zod.string().min(1),
-  });
-  const body = bodyValidator.parse(request.body);
-  await db
-    .insertInto('feedback')
-    .values({
-      name: body.name,
-      phoneNumber: body.phoneNumber,
-      text: body.text,
-    })
-    .executeTakeFirstOrThrow();
-
-  response.send({ success: true });
-  try {
-  } catch (error) {
-    console.error(error);
-    response.status(500).send({ success: false });
-  }
+  default:
+    if (request.method !== 'POST') {
+     return  response.status(404);
+    }
+    break;
+} 
 }
