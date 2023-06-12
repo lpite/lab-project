33e1d6820790dd9c28934609f6edade3e1d895b7
lpite <farionsasa@gmail.com>
2023-06-12 22:55:37 +0300

    final fixes

--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -17,17 +17,21 @@ import { clearCart } from '../store/cart/cart.actions';
 export class CheckoutPageComponent implements OnInit {
   paymentMethod: 'credit' | 'cash' = 'credit';
   checkOutForm = this.formBuilder.group({
-    name: [''],
+    name: ['', Validators.required],
     phoneNumber: [
       '+380',
       [Validators.required, Validators.pattern(/\+380\d{3}\d{2}\d{2}\d{2}/gi)],
     ],
-    promoCode: ['', Validators.required],
+    promoCode: [''],
     paymentMethod: ['credit'],
+    cardNumber: [''],
+    cardExpDate: [''],
+    cardCVC: [''],
   });
   pizzas$: Observable<CartPizza[]> = this.store.select(selectPizzas);
   cartTotalPrice$: Observable<number> = this.store.select(selectTotalPrice);
   discountPercentage: number = 0;
+  math = Math;
   constructor(
     private formBuilder: FormBuilder,
     private http: HttpClient,
@@ -44,7 +48,7 @@ export class CheckoutPageComponent implements OnInit {
   }
 
   checkPromoCode(): void {
-    if (this.checkOutForm.valid) {
+    if (this.checkOutForm.value.promoCode?.length) {
       this.http
         .get<{ valid: boolean; percentage: number }>(
           `/api/promocode/?promocode=${this.checkOutForm.value.promoCode}`
@@ -64,29 +68,38 @@ export class CheckoutPageComponent implements OnInit {
   }
 
   createOrder(): void {
-    console.log(this.checkOutForm.value.promoCode);
-    // TODO
-    const orderObject: any = {
-      paymentMethod: this.paymentMethod,
-      pizzas: [],
-      promocode: this.checkOutForm.getRawValue().promoCode || '',
-      customer: {
-        name: this.checkOutForm.value.name,
-        phoneNumber: this.checkOutForm.value.phoneNumber,
-      },
-    };
-    this.pizzas$.subscribe((pizzas) => {
-      orderObject.pizzas = pizzas;
-    });
-    this.http
-      .post<{ success: boolean }>('/api/order/', orderObject)
-      .subscribe(({ success }) => {
-        if (success) {
-          alert('Замовлення створено успішно!');
-          this.store.dispatch(clearCart());
-          this.router.navigate(['/']);
-        }
+    if (this.checkOutForm.value.paymentMethod === 'credit') {
+      const isCardNumberValid =
+        this.checkOutForm.value.cardNumber?.length === 23;
+      const isExpDateValid = this.checkOutForm.value.cardNumber?.length === 4;
+    }
+    if (this.checkOutForm.valid) {
+      // TODO
+
+      const orderObject: any = {
+        paymentMethod: this.paymentMethod,
+        pizzas: [],
+        promocode: this.checkOutForm.getRawValue().promoCode || '',
+        customer: {
+          name: this.checkOutForm.value.name,
+          phoneNumber: this.checkOutForm.value.phoneNumber,
+        },
+      };
+      this.pizzas$.subscribe((pizzas) => {
+        orderObject.pizzas = pizzas;
       });
+      this.http
+        .post<{ success: boolean }>('/api/order/', orderObject)
+        .subscribe(({ success }) => {
+          if (success) {
+            alert('Замовлення створено успішно!');
+            this.store.dispatch(clearCart());
+            this.router.navigate(['/']);
+          }
+        });
+    } else {
+      alert('Заповніть форму!');
+    }
   }
 
   validateInput(inputName: string): boolean {
