4abd5d11087c6bd0431c8f845f1a9feadcf16573
lpite <67955038+lpite@users.noreply.github.com>
2023-06-12 12:49:02 +0300

    Update order.ts

--- api/order.ts
+++ api/order.ts
@@ -125,7 +125,8 @@ export default async function (
         })
         .executeTakeFirstOrThrow();
     }
-
+  if(promo?.id){
+  
     await db
       .updateTable('promo_code')
       .where("id","=",promo.id)
@@ -133,6 +134,7 @@ export default async function (
         uses_left: (promo?.uses_left || 0) - 1,
       })
       .executeTakeFirstOrThrow();
+  }
     response.send({ success: true });
   } catch (error) {
     console.error(error);
