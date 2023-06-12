ef7f2be329275b196764461ca9dffe8404cb2f24
lpite <farionsasa@gmail.com>
2023-06-12 17:02:42 +0300

    revert

--- api/subscribe.ts
+++ api/subscribe.ts
@@ -30,49 +30,29 @@ export default async function (
   request: VercelRequest,
   response: VercelResponse
 ) {
-  
-switch (request.method) {
-  case "POST":
-    const bodyValidator = zod.object({
-      name: zod.string().regex(/[А-Яа-яЇїЄє ]/g),
-      phoneNumber: zod.string().regex(/\+380\d{3}\d{2}\d{2}\d{2}/gi),
-      text: zod.string().min(1),
-    });
-    const body = bodyValidator.parse(request.body);
-    await db
-      .insertInto('feedback')
-      .values({
-        name: body.name,
-        phoneNumber: body.phoneNumber,
-        text: body.text,
-      })
-      .executeTakeFirstOrThrow();
-  
-    response.send({ success: true });
-    try {
-    } catch (error) {
-      console.error(error);
-      response.status(500).send({ success: false });
-    }
-    break;
-    case "GET":
-      const subscriptions: any[] =  await db.selectFrom("feedback")
-      .selectAll()
-      .execute()
-      return response.send(subscriptions.map((el)=>` 
-      <div>
-        <h2>${el.name}</h2>
-        <h2>${el.phoneNumber}</h2>
-        <h4>${el.text}</h4>
-    </div>
-  `).join(""));
-      break;
+  if (request.method !== 'POST') {
+    response.status(404);
+  }
 
-  default:
-    if (request.method !== 'POST') {
-     return  response.status(404);
-    }
-    break;
-}
-  
+  const bodyValidator = zod.object({
+    name: zod.string().regex(/[А-Яа-яЇїЄє ]/g),
+    phoneNumber: zod.string().regex(/\+380\d{3}\d{2}\d{2}\d{2}/gi),
+    text: zod.string().min(1),
+  });
+  const body = bodyValidator.parse(request.body);
+  await db
+    .insertInto('feedback')
+    .values({
+      name: body.name,
+      phoneNumber: body.phoneNumber,
+      text: body.text,
+    })
+    .executeTakeFirstOrThrow();
+
+  response.send({ success: true });
+  try {
+  } catch (error) {
+    console.error(error);
+    response.status(500).send({ success: false });
+  }
 }
